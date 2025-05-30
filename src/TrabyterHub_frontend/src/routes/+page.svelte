3dbd363add188008c57b02e0e5d624a6a163c3c3
fGhost713 <8194847+fGhost713@users.noreply.github.com>
2025-05-30 23:36:40 +0200

    home page screen restored

--- src/TrabyterHub_frontend/src/routes/+page.svelte
+++ src/TrabyterHub_frontend/src/routes/+page.svelte
@@ -1,186 +1,186 @@
-<script>
+<script lang="ts">
+    let mainTextFontSize: string = '1.8em';
 </script>
 
-<table
-    cellspacing="0"
-    cellpadding="0"
-    style="height: 100%;width: 100%; font-size: 100%;"
->
-    <tbody>
-        <tr>
-            <!-- #region main page content area -->
-            <td style="width:100%;height:100%; ">
-                <div style="height: 100%;width:100%; ">
-                    <!-- <svg width="70.6em" height="65.1em" viewBox="0 0 70.6em 65.1em" fill="none" xmlns="http://www.w3.org/2000/svg"
-                style="display:flexbox;"
-                > -->
-                    <svg
-                        width="126.8rem"
-                        height="65.1rem"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlns:xlink="http://www.w3.org/1999/xlink"
-                    >
-                        <g>
-                            <path
-                                d="M724.619 1H1267.5V319H644L724.619 1Z"
-                                fill="#0E0F2C"
-                            />
-                            <path
-                                d="M724.619 1H1267.5V319H644L724.619 1Z"
-                                fill="#EDEDF3"
-                                fill-opacity="0.12"
-                            />
-                            <path
-                                d="M0 0.5H706L544.5 650.5H0V0.5Z"
-                                fill="#0E0F2C"
-                            />
-                            <path
-                                d="M640.619 333H1268V651H560L640.619 333Z"
-                                fill="#0E0F2C"
-                            />
-                            <path
-                                d="M640.619 333H1268V651H560L640.619 333Z"
-                                fill="#EDEDF3"
-                                fill-opacity="0.12"
-                            />
-                            <rect
-                                x="818"
-                                y="34"
-                                width="309"
-                                height="232"
-                                fill="url(#pattern0_0_1)"
-                            />
-                            <!-- <image src="../../icons/StakingTreeLogo.png" width="300px" height="300px"></image> -->
+<div>
+    <table cellspacing="0" cellpadding="0" style="height: 100%;width: 100%;">
+        <tbody>
+            <tr>
+                <!-- #region main page content area -->
+                <td style="width:100%;height:100%; ">
+                    <div style="height: 100%;width:100%; ">
+                        <!-- <svg width="70.6em" height="65.1em" viewBox="0 0 70.6em 65.1em" fill="none" xmlns="http://www.w3.org/2000/svg"
+    style="display:flexbox;"
+    > -->
+                        <svg
+                            width="142.0em"
+                            height="65.1em"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                        >
+                            <g>
+                                <path
+                                    d="M724.619 1H1267.5V319H644L724.619 1Z"
+                                    fill="#0E0F2C"
+                                />
+                                <path
+                                    d="M724.619 1H1267.5V319H644L724.619 1Z"
+                                    fill="#EDEDF3"
+                                    fill-opacity="0.12"
+                                />
+                                <path
+                                    d="M0 0.5H706L544.5 650.5H0V0.5Z"
+                                    fill="#0E0F2C"
+                                />
+                                <path
+                                    d="M640.619 333H1268V651H560L640.619 333Z"
+                                    fill="#0E0F2C"
+                                />
+                                <path
+                                    d="M640.619 333H1268V651H560L640.619 333Z"
+                                    fill="#EDEDF3"
+                                    fill-opacity="0.12"
+                                />
+                                <rect
+                                    x="30em"
+                                    y="34em"
+                                    width="309"
+                                    height="232"
+                                    fill="url(#pattern0_0_1)"
+                                />
+                                <!-- <image src="../../icons/StakingTreeLogo.png" width="300px" height="300px"></image> -->
 
-                            <foreignobject
-                                x="82.0rem"
-                                y="3em"
-                                width="32rem"
-                                height="32rem"
-                                style="line-height: 0.4em;"
-                            >
-                                <image
-                                    class="image-Nft-staking-logo"
-                                    style="width: 32em;height:24em;opacity: 0.85;"
-                                ></image>
-                                <p
-                                    style="font-size: 1.75rem; color:rgb(237, 168, 89); opacity:0.9;text-align:center"
+                                <foreignobject
+                                    x="50.0em"
+                                    y="2.0em"
+                                    width="22em"
+                                    height="24em"
+                                    style="line-height: 0.9em;"
                                 >
-                                    To stake or not to stake ?
-                                </p>
-                            </foreignobject>
-
-                            <foreignobject
-                                x="82.0rem"
-                                y="35rem"
-                                width="30rem"
-                                height="32rem"
-                                style="line-height: 0.4rem;"
-                            >
-                                <image
-                                    class="image-Nft-TradeByBarter-logo"
-                                    style="width: 29em;height:24em;opacity: 0.85;"
-                                ></image>
-                                <p
-                                    style="margin-top:1.5em;font-size: 1.75em; color:rgb(237, 168, 89); opacity:0.9;text-align:center"
-                                >
-                                    Bundled Nft buy - And buy more...
-                                </p>
-                            </foreignobject>
-
-                            <foreignobject
-                                x="3.0rem"
-                                y="1em"
-                                width="50em"
-                                height="60em"
-                                style="line-height: 0.4em;"
-                            >
-                                <p
-                                    style="font-size: 2.75em; color:red; opacity:0.9;margin-bottom:2.0em "
-                                >
-                                    Decentralized
-                                    <font style="color: white;"
-                                        >Trade By Barter Hub</font
+                                    <img
+                                        src="/images/StakingAppTreeLogo.png"
+                                        style="width: 22em;height:15em;opacity: 0.85;"
+                                        alt="Staking App Tree Logo"
+                                    />
+                                    <p
+                                        style="font-size: 1.0em; color:rgb(237, 168, 89); opacity:0.9;text-align:center"
                                     >
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
+                                        To stake or not to stake ?
+                                    </p>
+                                </foreignobject>
+
+                                <foreignobject
+                                    x="50.0em"
+                                    y="22.0em"
+                                    width="22em"
+                                    height="24em"
+                                    style="line-height: 0.9em;"
                                 >
-                                    We have revived the time-honored
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
+                                    <img
+                                        src="/images/NftMarketAppBuyLogo.png"
+                                        style="width: 19em;height:15em;opacity: 0.85;"
+                                        alt="Nft Market App Tree Logo"
+                                    />
+                                    <p
+                                        style="font-size: 1.0em; color:rgb(237, 168, 89); opacity:0.9;text-align:center"
+                                    >
+                                        Bundled Nft buy - And buy more...
+                                    </p>
+                                </foreignobject>
+
+                                <foreignobject
+                                    x="1.5em"
+                                    y="0.5em"
+                                    width="40em"
+                                    height="60em"
+                                    style="line-height: 0.4em;"
                                 >
-                                    practice of Trade By Barter within
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    our marketplace. Here,
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    participants have the opportunity
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    to leverage their current NFTs/
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    Tokens by bundling them and
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    exchanging them for desired items
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    that are currently beyond their
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    reach. Furthermore, our platform
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    facilitates NFT staking and peer-to-
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    peer token transactions without
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    the requirement for initial liquidity
-                                </p>
-                                <p
-                                    style="font-size: 2.75em; color:White; opacity:0.9"
-                                >
-                                    or volume.
-                                </p>
-                            </foreignobject>
-                        </g>
-                    </svg>
-                </div>
-            </td>
-            <!-- #endregion main page content area -->
-        </tr>
-        <tr style="height: 2em;"> </tr>
-    </tbody>
-</table>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:red; opacity:0.9;margin-bottom:2.0em "
+                                    >
+                                        Decentralized
+                                        <font style="color: white;"
+                                            >Trade By Barter Hub</font
+                                        >
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        We have revived the time-honored
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        practice of Trade By Barter within
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        our marketplace. Here,
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        participants have the opportunity
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        to leverage their current NFTs/
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        Tokens by bundling them and
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        exchanging them for desired items
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        that are currently beyond their
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        reach. Furthermore, our platform
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        facilitates NFT staking and peer-to-
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        peer token transactions without
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        the requirement for initial liquidity
+                                    </p>
+                                    <p
+                                        style="font-size: {mainTextFontSize}; color:White; opacity:0.9"
+                                    >
+                                        or volume.
+                                    </p>
+                                </foreignobject>
+                            </g>
+                        </svg>
+                    </div>
+                </td>
+                <!-- #endregion main page content area -->
+            </tr>
+            <tr style="height: 2em;"> </tr>
+        </tbody>
+    </table>
+</div>
 
 <!-- The contents of the Home Page -->
 
-<style>
-</style>
+<style src="./homePageStyles.css"></style>
